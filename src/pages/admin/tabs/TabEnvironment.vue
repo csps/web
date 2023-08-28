dfeaa2f252b2cfe970cb62733baa359181b63f5b
mavyfaby <maverickfabroa@gmail.com>
2023-08-28 13:55:54 +0800

    feat(admin): implement search env

--- src/pages/admin/tabs/TabEnvironment.vue
+++ src/pages/admin/tabs/TabEnvironment.vue
@@ -2,7 +2,7 @@
   <div class="px-6">
 
     <div class="flex flex-col items-center justify-center mb-2 gap-3">
-      <md-outlined-text-field label="Search variable">
+      <md-outlined-text-field v-model="search" label="Search variable">
         <md-icon slot="leadingicon" v-html="icon('search')" />
       </md-outlined-text-field>
 
@@ -13,7 +13,7 @@
     </div>
 
     <div class="flex flex-col items-center pt-6 gap-3">
-      <div v-for="item in Object.entries(Env)" :key="item[0]" @click="onEnvClick(item)" class="item text-on-surface-variant" role="button">
+      <div v-for="item in list" :key="item[0]" @click="onEnvClick(item)" class="item text-on-surface-variant" role="button">
         <md-ripple />
         <div class="flex justify-between gap-6 items-center">
           <div>
@@ -42,14 +42,31 @@ import { capitalize } from "~/utils/string";
 import DialogAdminEnv from "~/components/dialogs/DialogAdminEnv.vue";
 
 const isDialogOpen = ref(false);
+const list = ref(Object.entries(Env));
+const search = ref("");
 const name = ref("");
 const value = ref("");
 
 watch(isDialogOpen, v => {
   if (!v) {
-    name.value = "";
-    value.value = "";
+    setTimeout(() => {
+      name.value = "";
+      value.value = "";
+    }, 300);
   }
+});
+
+watch(search, v => {
+  if (v) {
+    list.value = Object.entries(Env).filter(
+      item => capitalize(item[0]).toLowerCase().includes(v.toLowerCase()) ||
+        item[1].toString().toLowerCase().includes(v.toLowerCase())
+    );
+
+    return;
+  }
+
+  list.value = Object.entries(Env);
 });
 
 function onEnvClick(item: [string, string | number]) {

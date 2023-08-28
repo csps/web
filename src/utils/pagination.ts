c94583c8cb820bcea7c63bf39e437d2adf7967ac
mavyfaby <maverickfabroa@gmail.com>
2023-08-28 18:33:39 +0800

    feat(app): add pagination algorithm by @jorrit91

---
+++ src/utils/pagination.ts
@@ -0,0 +1,48 @@
+const getRange = (start: number, end: number) => {
+  return Array.from(Array(end - start + 1), (_, i) => start + i);
+}
+
+/**
+ * Pagination algorithm
+ * @author jorrit91
+ * @param {number} currentPage
+ * @param {number} pageCount
+ */
+export const pagination = (currentPage: number, pageCount: number): (string|number)[] => {
+  let delta: number
+  if (pageCount <= 7) {
+    // delta === 7: [1 2 3 4 5 6 7]
+    delta = 7
+  } else {
+    // delta === 2: [1 ... 4 5 6 ... 10]
+    // delta === 4: [1 2 3 4 5 ... 10]
+    delta = currentPage > 4 && currentPage < pageCount - 3 ? 2 : 4
+  }
+
+  const range = {
+    start: Math.round(currentPage - delta / 2),
+    end: Math.round(currentPage + delta / 2)
+  }
+
+  if (range.start - 1 === 1 || range.end + 1 === pageCount) {
+    range.start += 1
+    range.end += 1
+  }
+
+  let pages: any =
+    currentPage > delta
+      ? getRange(Math.min(range.start, pageCount - delta), Math.min(range.end, pageCount))
+      : getRange(1, Math.min(pageCount, delta + 1))
+
+  const withDots = (value: number, pair: any[]) => (pages.length + 1 !== pageCount ? pair : [value])
+
+  if (pages[0] !== 1) {
+    pages = withDots(1, [1, '...']).concat(pages)
+  }
+
+  if (pages[pages.length - 1] < pageCount) {
+    pages = pages.concat(withDots(pageCount, ['...', pageCount]))
+  }
+
+  return pages;
+}

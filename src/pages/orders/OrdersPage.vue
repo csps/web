c4e1d134bdb0e13f68648322d3b3d09c15788edb
mavyfaby <maverickfabroa@gmail.com>
2023-09-01 09:25:29 +0800

    style(orders): centered elements

--- src/pages/orders/OrdersPage.vue
+++ src/pages/orders/OrdersPage.vue
@@ -5,30 +5,30 @@
         My orders
       </h2>
       <h6 class="text-sm">
-        To view your orders, please enter your receipt ID and student ID.
+        To view your orders, please enter your receipt and student ID.
       </h6>
-    </div>
 
-    <div class="flex flex-col gap-6 mt-5">
-      <md-outlined-text-field @keydown.enter="submit" v-model.trim="receiptId" label="Receipt ID" prefix-text="CSPS">
-        <md-icon slot="leadingicon" v-html="icon('receipt', true)" />
-      </md-outlined-text-field>
-      <md-outlined-text-field @keydown.enter="submit" v-model.trim="studentId" label="Student ID" type="number">
-        <md-icon slot="leadingicon" v-html="icon('badge', true)" />
-      </md-outlined-text-field>
-
-      <md-filled-button @click="submit" :disabled="receiptId.length === 0 || studentId.length === 0">
-        View order
-      </md-filled-button>
-
-      <p class="body-small text-center">
-        Have an account? 
-        <router-link :to="{ name: 'Login' }">
-          Click here
-        </router-link>
-
-        to login  and view your orders!
-      </p>
+      <div class="flex flex-col gap-6 mt-5">
+        <md-outlined-text-field @keydown.enter="submit" v-model.trim="receiptId" label="Receipt ID" prefix-text="CSPS">
+          <md-icon slot="leadingicon" v-html="icon('receipt', true)" />
+        </md-outlined-text-field>
+        <md-outlined-text-field @keydown.enter="submit" v-model.trim="studentId" label="Student ID" type="number">
+          <md-icon slot="leadingicon" v-html="icon('badge', true)" />
+        </md-outlined-text-field>
+  
+        <md-filled-button @click="submit" :disabled="receiptId.length === 0 || studentId.length === 0">
+          View order
+        </md-filled-button>
+  
+        <p class="body-small text-center">
+          Have an account? 
+          <router-link :to="{ name: 'Login' }">
+            Click here
+          </router-link>
+  
+          to login  and view your orders!
+        </p>
+      </div>
     </div>
   </div>
 </template>

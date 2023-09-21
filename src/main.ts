6c5ab494cea9ace97517ed5a23aa7ebceb64e56e
mavyfaby <maverickfabroa@gmail.com>
2023-09-21 21:03:41 +0800

    chore(app): add support for older versions of iOS safari browsers

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from "pinia";
 import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify';
+import "element-internals-polyfill";
 
 import App from './App.vue'
 import router from './router'
